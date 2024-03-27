7e38ac6de738ccad5884771365ea63a9e425c026
Tài Đinh <99629219+taidt21@users.noreply.github.com>
2024-03-27 17:34:49 +0700

    update header

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
     $('header').load('header.html');
     // $('footer').load('footer.html');
+
+setTimeout(()=>{
+    
+    $('.btn-open-poppup').click(function(){
+        $('.poppup-mobile').addClass('active');
+        $('.overlay').addClass('active');
+    })
+    $('.overlay').click(function(){
+        $(this).removeClass('active');
+        $('.poppup-mobile').removeClass('active');
+    })
+},1000)
 })
